f8cc2e482547f9bf5dcbdb5598b6256990bdf716
omarChouikhEspritAL <omar.chouikh@esprit.tn>
2025-06-25 00:16:52 +0100

    fix gateway, stablizie kafka send messages in microservices

--- Ms_Event/src/main/java/esprit/tn/ms_event/Dto/EventDto.java
+++ Ms_Event/src/main/java/esprit/tn/ms_event/Dto/EventDto.java
@@ -1,7 +1,14 @@
 package esprit.tn.ms_event.Dto;
 
-import java.sql.Date;
+
 import java.time.LocalDate;
 
 public record EventDto(String id, String name, LocalDate dateEvent) {
+    public EventDto withName(String name) {
+        return new EventDto(this.id, name, this.dateEvent);
+    }
+
+    public EventDto withDateEvent(LocalDate dateEvent) {
+        return new EventDto(this.id, this.name, dateEvent);
+    }
 }
