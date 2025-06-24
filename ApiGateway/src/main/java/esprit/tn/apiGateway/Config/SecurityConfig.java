d1f11e7eecae03d6734c7c8bc6a5847b8353a8f1
omarChouikhEspritAL <omar.chouikh@esprit.tn>
2025-06-24 19:23:59 +0100

    add apiGateWay, correct dockercompose file

---
+++ ApiGateway/src/main/java/esprit/tn/apiGateway/Config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package esprit.tn.apiGateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//@Configuration
+//@EnableWebFluxSecurity
+public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        http
+//                .authorizeExchange()
+//                .pathMatchers("/api/prospects/**", "/api/events/**").authenticated()
+//                .anyExchange().permitAll()
+//                .and()
+//                .oauth2Login()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//        return http.build();
+//    }
+}
---
+++ ApiGateway/src/test/java/esprit/tn/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.tn.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
