9df59249c751f78bb011c850f157130c53f24233
omarChouikhEspritAL <omar.chouikh@esprit.tn>
2025-06-26 02:28:26 +0100

    add keycloak to api-gateway tested ok

--- ApiGateway/src/main/java/esprit/tn/apiGateway/Config/SecurityConfig.java
+++ ApiGateway/src/main/java/esprit/tn/apiGateway/Config/SecurityConfig.java
@@ -1,25 +1,28 @@
 package esprit.tn.apiGateway.Config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//@Configuration
-//@EnableWebFluxSecurity
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-//        http
-//                .authorizeExchange()
-//                .pathMatchers("/api/prospects/**", "/api/events/**").authenticated()
-//                .anyExchange().permitAll()
-//                .and()
-//                .oauth2Login()
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt();
-//        return http.build();
-//    }
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/prospects/**", "/events/**").authenticated()
+                        .anyExchange().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
 }
